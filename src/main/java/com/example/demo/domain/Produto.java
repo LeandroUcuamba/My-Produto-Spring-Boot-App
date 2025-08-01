27dc2cf5c6f90c3e67958d5077247babc11e2aaf
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-08-01 22:31:40 +0100

    entidade de dominio e repository adicionado

---
+++ src/main/java/com/example/demo/domain/Produto.java
@@ -0,0 +1,57 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+    @Id
+    private String id;
+    private String nome;
+    private String descricao;
+    private Double valor;
+    private Integer quantidade;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
